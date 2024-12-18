9400a7592b227306c752bdd75aef4eff358d0350
dattrinht <dattrinh.net@gmail.com>
2024-12-18 14:13:14 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const nextConfig = {
     images: {
         domains: ["i.ibb.co"],
     },
+    experimental: {
+        serverActions: {
+            allowedOrigins: ['localhost:3005', '192.168.80.10:3005', 'wesendemail.online'],
+        },
+    },
 };
 
 export default nextConfig;
